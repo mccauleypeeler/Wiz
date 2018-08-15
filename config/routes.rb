4faa9cf23c8ef5e37ca5b9820a19d72bf81f4119
McCauley Peeler <mccauleypeeler@gmail.com>
2018-08-15 12:59:50 -0400

    landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :wizards
   devise_for :users
-  root 'welcome#show'
+  root 'landing#home'
   get 'show', to: 'welcome#show', as: 'show_dash'
   put 'toggle', to: 'welcome#tog', as: 'toggle_button'
 
-  get '/dash', to: 'wizard_dashboard#dash'
+  get '/dash', to: 'wizard_dashboard#dash', as: 'dashboard'
   post '/dash', to: 'wizard_dashboard#claim'
   get 'number', to: 'wizard_dashboard#number'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
